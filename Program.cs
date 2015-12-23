f879ff20f29ea7a47785fa14e55fdce9fe07599d
Tim Marsh <t.marsh@my.westminster.ac.uk>
2015-12-23 21:42:38 +0000

    Refactoring code to generate screenshot

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -7,14 +10,208 @@ using System.Linq;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
+using TechTalk.SpecFlow;
 
 namespace TimMarsh.Screenshot
 {
     public class Screenshot
-    {   
-        public static void TakeScreenshot()
+    {
+        protected static IWebDriver Driver;
+
+        private static string ScreenshotPath
         {
+            get
+            {
+                if (!String.IsNullOrEmpty(ConfigurationManager.AppSettings["ScreenshotPath"]))
+                {
+                    return ConfigurationManager.AppSettings["ScreenshotPath"];
+                }
+                return AppDomain.CurrentDomain.BaseDirectory + "Screenshots\\";
+            }
+        }
+
+        private static string BaseUrl
+        {
+            get
+            {
+                if (!String.IsNullOrEmpty(ConfigurationManager.AppSettings["BaseUrl"]))
+                {
+                    return ConfigurationManager.AppSettings["BaseUrl"];
+                }
+                return "http://main-vnext.uel.ac.uk";
+            }
+        }
+
+        public static string FilePath
+        {
+            get
+            {
+                return ScreenshotPath + FileName;
+            }
+        }
+
+        private static string _fileName;
+        private static string FileName
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(_fileName))
+                {
+                    string file = Driver.Url
+                        .Replace(BaseUrl, "")
+                        .Replace(@"http://www.uel.ac.uk", "")
+                        .Replace(@".ac.uk", "")
+                        .Replace("/", @"\");
+
+                    _fileName = file.Substring(0, file.IndexOf("?") > 0 ? file.IndexOf("?") : file.Length);
+                }
+                return _fileName;
+            }
+        }
+
+        public static void TakeScreenshot(string url)
+        {
+            // Open the browser and navigate to the item
+            NavigateToWebPage(url);
+
             // Take Screenshot code goes here
+            bool exists = Directory.Exists(FilePath);
+
+            if (!exists)
+                Directory.CreateDirectory(Path.Combine(FilePath));
+
+            string fileName = Path.Combine(FilePath, DateTime.Now.ToString(@"MMM-ddd-d-HH.mm.ss") + ".png");
+                        
+            Bitmap image = GetEntireScreenshot();
+            image.Save(fileName, ImageFormat.Png);
+            image.Dispose();
+        }
+
+        private static void NavigateToWebPage(string url)
+        {
+            if (!ScenarioContext.Current.ContainsKey("BaseUrl"))
+            {
+                //BaseUrl = url;
+                //ScenarioContext.Current.Add("BaseUrl", BaseUrl);
+            }
+            ////----Server----////
+            DesiredCapabilities capability = DesiredCapabilities.Chrome();
+            Driver = new RemoteWebDriver(new Uri("http://dl-sc-bad:5558/wd/hub/"), capability);
+
+            Driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+            Driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(30));
+            Driver.Manage().Window.Maximize();
+        }
+
+        public static Bitmap GetEntireScreenshot()
+        {
+
+            Bitmap stitchedImage = null;
+            try
+            {
+                long totalwidth1 = (long)((IJavaScriptExecutor)Driver).ExecuteScript("return document.body.offsetWidth");//documentElement.scrollWidth");
+
+                long totalHeight1 = (long)((IJavaScriptExecutor)Driver).ExecuteScript("return  document.body.parentNode.scrollHeight");
+
+                int totalWidth = (int)totalwidth1;
+                int totalHeight = (int)totalHeight1;
+
+                // Get the Size of the Viewport
+                long viewportWidth1 = (long)((IJavaScriptExecutor)Driver).ExecuteScript("return document.body.clientWidth");//documentElement.scrollWidth");
+                long viewportHeight1 = (long)((IJavaScriptExecutor)Driver).ExecuteScript("return window.innerHeight");//documentElement.scrollWidth");
+
+                int viewportWidth = (int)viewportWidth1;
+                int viewportHeight = (int)viewportHeight1;
+
+
+                // Split the Screen in multiple Rectangles
+                List<Rectangle> rectangles = new List<Rectangle>();
+                // Loop until the Total Height is reached
+                for (int i = 0; i < totalHeight; i += viewportHeight)
+                {
+                    int newHeight = viewportHeight;
+                    // Fix if the Height of the Element is too big
+                    if (i + viewportHeight > totalHeight)
+                    {
+                        newHeight = totalHeight - i;
+                    }
+                    // Loop until the Total Width is reached
+                    for (int ii = 0; ii < totalWidth; ii += viewportWidth)
+                    {
+                        int newWidth = viewportWidth;
+                        // Fix if the Width of the Element is too big
+                        if (ii + viewportWidth > totalWidth)
+                        {
+                            newWidth = totalWidth - ii;
+                        }
+
+                        // Create and add the Rectangle
+                        Rectangle currRect = new Rectangle(ii, i, newWidth, newHeight);
+                        rectangles.Add(currRect);
+                    }
+                }
+
+                // Build the Image
+                stitchedImage = new Bitmap(totalWidth, totalHeight);
+                // Get all Screenshots and stitch them together
+                Rectangle previous = Rectangle.Empty;
+
+                foreach (var rectangle in rectangles)
+                {
+                    // Calculate the Scrolling (if needed)
+                    if (previous != Rectangle.Empty)
+                    {
+                        int xDiff = rectangle.Right - previous.Right;
+                        int yDiff = rectangle.Bottom - previous.Bottom;
+                        // Scroll
+                        ((IJavaScriptExecutor)Driver).ExecuteScript(String.Format("window.scrollBy({0}, {1})", xDiff, yDiff));
+
+                        // ------- UEL Specific Items ---------------
+                        if (Driver.Url.ToLower().Contains("uel"))
+                        {
+                            //if ((bool)((IJavaScriptExecutor)Driver).ExecuteScript("return document.getElementById('js-uel-top-nav')) != null"));
+                            ((IJavaScriptExecutor)Driver).ExecuteScript("$('#js-uel-top-nav').attr('style', 'display: none')");
+                            ((IJavaScriptExecutor)Driver).ExecuteScript("$('.sc-second_navigation').attr('style', 'visibility: hidden')");
+                            ((IJavaScriptExecutor)Driver).ExecuteScript("$('.sc-cookie-warning').attr('style', 'visibility: hidden')");
+                            ((IJavaScriptExecutor)Driver).ExecuteScript("$('.js-fixed__elements.ci-fixed__elements.fx-desktop-active').attr('style', 'visibility: hidden')");
+                        }
+                        Thread.Sleep(200);
+                    }
+
+                    // Take Screenshot
+                    var screenshot = ((ITakesScreenshot)Driver).GetScreenshot();
+
+                    // Build an Image out of the Screenshot
+                    Image screenshotImage;
+                    using (MemoryStream memStream = new MemoryStream(screenshot.AsByteArray))
+                    {
+                        screenshotImage = Image.FromStream(memStream);
+                    }
+
+                    // Calculate the Source Rectangle
+                    Rectangle sourceRectangle = new Rectangle(viewportWidth - rectangle.Width, viewportHeight - rectangle.Height, rectangle.Width, rectangle.Height);
+
+                    // Copy the Image
+                    using (Graphics g = Graphics.FromImage(stitchedImage))
+                    {
+                        g.DrawImage(screenshotImage, rectangle, sourceRectangle, GraphicsUnit.Pixel);
+                    }
+
+                    // Set the Previous Rectangle
+                    previous = rectangle;
+                }
+            }
+            catch (Exception ex)
+            {
+                // handle
+            }
+            return ResizeImage(stitchedImage, new Size(stitchedImage.Width / 2, stitchedImage.Height / 2));
+        }
+
+        public static Bitmap ResizeImage(Image imgToResize, Size size)
+        {
+            return new Bitmap(imgToResize, size);
         }
 
         public static void CompareScreenshot()
@@ -23,6 +220,7 @@ namespace TimMarsh.Screenshot
             sb.AppendLine("Screenshot comparison tool");
             sb.AppendLine("Tim Marsh - UEL Web Team");
             sb.AppendLine("------------------------------");
+
 
             var sourceDirectory = new DirectoryInfo(@"\\DL-SCT-TST\Screenshots");
             var outputDirectory = new DirectoryInfo(@"\\DL-SCT-TST\Screenshots\Output\" + DateTime.Now.ToString(@"ddMMyyHHmmss")).FullName;
@@ -328,7 +526,7 @@ namespace TimMarsh.Screenshot
 
         static void Main(string[] args)
         {
-            Screenshot.CompareScreenshot();
+            Screenshot.TakeScreenshot("http://www.uel.ac.uk");
         }
 
         private static void BrokenLinkCheck(StringBuilder sb)
